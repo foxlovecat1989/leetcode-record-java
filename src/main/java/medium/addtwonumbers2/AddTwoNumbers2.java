a9b9256a040df9926927fef838c15e1802931013
foxlovecatii <foxlovecat1989@gmail.com>
2023-11-26 17:22:11 +0800

    Add Two Numbers 2 - rename

--- src/main/java/medium/addtwonumbers2/AddTwoNumbers2.java
+++ src/main/java/medium/addtwonumbers2/AddTwoNumbers2.java
@@ -33,12 +33,12 @@ public class AddTwoNumbers2 {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode(0, null);
         ListNode temp = ans;
-        boolean isCarried = false;
+        boolean isOverflowed = false;
         while (l1 != null || l2 != null) {
             int value = (l1 != null ? l1.val : 0)
                     + (l2 != null ? l2.val : 0)
-                    + (isCarried ? 1 : 0);
-            isCarried = value >= 10;
+                    + (isOverflowed ? 1 : 0);
+            isOverflowed = value >= 10;
             value = value % 10;
             l1 = l1 == null ? null : l1.next;
             l2 = l2 == null ? null : l2.next;
@@ -46,8 +46,8 @@ public class AddTwoNumbers2 {
             temp = temp.next;
         }
 
-        // append when last one node carry happen
-        if (isCarried) {
+        // append one node with value one when last one node addition overflow
+        if (isOverflowed) {
             temp.next = new ListNode(1, null);
         }
 
